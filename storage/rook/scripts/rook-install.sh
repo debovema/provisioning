1092e4f3af345c6bf677957f3c697abb655e047b
Mathieu Debove <debovemathieu@gmail.com>
2018-03-05 00:33:34 +0100

    Add delay to wait for Rook operator to respond

--- storage/rook/scripts/rook-install.sh
+++ storage/rook/scripts/rook-install.sh
@@ -4,6 +4,7 @@ kubectl apply -f /tmp/rook/rook-operator.yaml
 
 while [ ! "$(kubectl get pods -n rook-system | grep Running | wc -l)" -eq "3" ]; do
   echo "Waiting for Rook operator to be running"
+  sleep 5
 done
 
 echo "Rook operator is running"
